b0a7d5649eb49877fd627ce8c82d536f4f3310b8
Diego Alfonzo <149984303+dalfonzo21@users.noreply.github.com>
2024-11-08 23:31:29 -0500

    commit propiedad

--- tarea01diseniolarga/src/main/java/ec/edu/espol/tarea01diseniolarga/Propiedad.java
+++ tarea01diseniolarga/src/main/java/ec/edu/espol/tarea01diseniolarga/Propiedad.java
@@ -11,23 +11,24 @@ import java.util.ArrayList;
  * @author marsa
  */
 public class Propiedad {
-    int id;
-    String direccion;
-    TipoPropiedad tipo;
-    ArrayList<Unidad> unidades = new ArrayList<>();
-    ReglasPropiedad reglas;
-    double calificacion;
+    private int id;
+    private String direccion;
+    private TipoPropiedad tipo;
+    private ArrayList<Unidad> unidades = new ArrayList<>();
+    private ReglasPropiedad reglas;
+    private double calificacion;
 
     void verUnidades() {
         // Lógica para ver unidades
     }
 
-    void agregarUnidad() {
+    void agregarUnidad(Unidad unidad) {
         // Lógica para agregar unidad
     }
 
-    void modificarReglas() {
+    void modificarReglas(ReglaPropiedad nuevaRegla) {
         // Lógica para modificar reglas
+        this.reglas=nuevaRegla;
     }
     
 }
