c54b7d1718dbab190cad4494fa16f3c7092704ec
MARLO VALLE <mcvalle@espol.edu.ec>
2024-11-08 23:20:12 -0500

    Create Resenia.java

---
+++ tarea01diseniolarga/src/main/java/ec/edu/espol/tarea01diseniolarga/Resenia.java
@@ -0,0 +1,15 @@
+package ec.edu.espol.tarea01diseniolarga;
+
+import java.sql.Date;
+
+public class Resenia {
+    private int id;
+    private double calificacion;
+    private String comentario;
+    private Date fecha;
+
+    public void crearReseña() {
+        // Código para crear reseña
+    }
+
+}
