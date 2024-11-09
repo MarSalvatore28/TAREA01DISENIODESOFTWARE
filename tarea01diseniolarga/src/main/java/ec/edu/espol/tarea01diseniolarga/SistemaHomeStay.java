03574071b254506615b94c1e9666f60272667ba8
MARLO VALLE <mcvalle@espol.edu.ec>
2024-11-08 23:54:30 -0500

    Update SistemaHomeStay.java

--- tarea01diseniolarga/src/main/java/ec/edu/espol/tarea01diseniolarga/SistemaHomeStay.java
+++ tarea01diseniolarga/src/main/java/ec/edu/espol/tarea01diseniolarga/SistemaHomeStay.java
@@ -1,6 +1,27 @@
 package ec.edu.espol.tarea01diseniolarga;
 
 public class SistemaHomeStay {
+    private ServicioDeNotificacion servicioDeNotificacion;
+
+    public SistemaHomeStay() {
+        this.servicioDeNotificacion = new ServicioDeNotificacion();
+    }
+
+    public void realizarAccion(String tipoAccion) {
+        System.out.println("Acción realizada: " + tipoAccion);
+        String mensaje = generarMensaje(tipoAccion);
+        servicioDeNotificacion.enviarNotificacion(mensaje);
+        System.out.println("Notificación enviada.");
+    }
+
+    private String generarMensaje(String tipoAccion) {
+        return "Mensaje relacionado a: " + tipoAccion;
+    }
+
+    public static void main(String[] args) {
+        SistemaHomeStay sistema = new SistemaHomeStay();
+        sistema.realizarAccion("Confirmar Reserva");
+    }
     public void aplicarFiltro() {
         // Código para aplicar filtros de búsqueda
         System.out.println("Filtro aplicado");
