86b36ae186e1eaf765fdb6d3c8ae1c585a33779d
MARLO VALLE <mcvalle@espol.edu.ec>
2024-11-08 23:14:55 -0500

    Create SistemaHomeStay.java

---
+++ tarea01diseniolarga/src/main/java/ec/edu/espol/tarea01diseniolarga/SistemaHomeStay.java
@@ -0,0 +1,19 @@
+package ec.edu.espol.tarea01diseniolarga;
+
+public class SistemaHomeStay {
+    public void aplicarFiltro() {
+        // Código para aplicar filtros de búsqueda
+        System.out.println("Filtro aplicado");
+    }
+
+    public void mostrarPropiedadesDestacadas() {
+        // Código para mostrar propiedades destacadas
+        System.out.println("Propiedades destacadas mostradas");
+    }
+
+    public void verificarDisponibilidad() {
+        // Código para verificar disponibilidad de las propiedades
+        System.out.println("Disponibilidad verificada");
+    }
+
+}
